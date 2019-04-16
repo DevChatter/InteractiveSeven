ed9650303b82e971d437c83c0094dd422cd7989d
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-15 23:32:47 -0400

    Initial WinForms App

---
+++ InteractiveSeven/InteractiveSeven.UI/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace InteractiveSeven.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExeLabel = new System.Windows.Forms.Label();
+            this.ExeTextBox = new System.Windows.Forms.TextBox();
+            this.ExeBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // ExeLabel
+            // 
+            this.ExeLabel.AutoSize = true;
+            this.ExeLabel.Location = new System.Drawing.Point(13, 13);
+            this.ExeLabel.Name = "ExeLabel";
+            this.ExeLabel.Size = new System.Drawing.Size(100, 13);
+            this.ExeLabel.TabIndex = 0;
+            this.ExeLabel.Text = "FF7 Executable File";
+            // 
+            // ExeTextBox
+            // 
+            this.ExeTextBox.Location = new System.Drawing.Point(120, 13);
+            this.ExeTextBox.Name = "ExeTextBox";
+            this.ExeTextBox.Size = new System.Drawing.Size(259, 20);
+            this.ExeTextBox.TabIndex = 1;
+            // 
+            // ExeBrowse
+            // 
+            this.ExeBrowse.Location = new System.Drawing.Point(397, 13);
+            this.ExeBrowse.Name = "ExeBrowse";
+            this.ExeBrowse.Size = new System.Drawing.Size(75, 23);
+            this.ExeBrowse.TabIndex = 2;
+            this.ExeBrowse.Text = "Browse";
+            this.ExeBrowse.UseVisualStyleBackColor = true;
+            this.ExeBrowse.Click += new System.EventHandler(this.ExeBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExeBrowse);
+            this.Controls.Add(this.ExeTextBox);
+            this.Controls.Add(this.ExeLabel);
+            this.Name = "Form1";
+            this.Text = "Interative Seven";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ExeLabel;
+        private System.Windows.Forms.TextBox ExeTextBox;
+        private System.Windows.Forms.Button ExeBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ InteractiveSeven/InteractiveSeven.UI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InteractiveSeven.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExeBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ExeTextBox.Text = openFileDialog1.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
