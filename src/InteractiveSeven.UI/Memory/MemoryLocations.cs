d28556fad707949a159491a9a04a37c87cf3ba15
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-18 18:26:25 -0400

    Fixing build error

--- src/InteractiveSeven.UI/Memory/MemoryLocations.cs
+++ src/InteractiveSeven.UI/Memory/MemoryLocations.cs
@@ -2,26 +2,52 @@
 
 namespace InteractiveSeven.UI.Memory
 {
-    public static class MemoryLocations
+    public class MemLoc
     {
-        public static readonly IntPtr TopLeftAddress = new IntPtr(0x91EFC8); // 3 sequential bytes blue, green, red
-        public static readonly IntPtr BotLeftAddress = new IntPtr(0x91EFCC);
-        public static readonly IntPtr TopRightAddress = new IntPtr(0x91EFD0);
-        public static readonly IntPtr BotRightAddress = new IntPtr(0x91EFD4);
+        public IntPtr Address { get; set; }
+        public int NumBytes { get; set; }
+        private MemLoc(int address, int numBytes = 1)
+        {
+            Address = new IntPtr(address);
+            NumBytes = numBytes;
+        }
 
-        public static readonly IntPtr CloudLimitAddress = new IntPtr(0xDBFD9B); // byte
-        public static readonly IntPtr BarretLimitAddress = new IntPtr(0xDBFE1F); // byte
-        public static readonly IntPtr TifaLimitAddress = new IntPtr(0xDBFEA3); // byte
+        public static readonly MemLoc MenuTopLeft = new MemLoc(0x91EFC8, 3); // order blue, green, red
+        public static readonly MemLoc MenuBotLeft = new MemLoc(0x91EFCC, 3);
+        public static readonly MemLoc MenuTopRight = new MemLoc(0x91EFD0, 3);
+        public static readonly MemLoc MenuBotRight = new MemLoc(0x91EFD4, 3);
 
-        public static readonly IntPtr CloudMaxHPAddress = new IntPtr(0xDBA4AA); // 2 bytes
-        public static readonly IntPtr CloudCurHPAddress = new IntPtr(0xDBFDB8); // 2 bytes
+        public static readonly MemLoc CloudLimit = new MemLoc(0xDBFD9B);
+        public static readonly MemLoc BarretLimit = new MemLoc(0xDBFE1F);
+        public static readonly MemLoc TifaLimit = new MemLoc(0xDBFEA3);
 
-        public static readonly IntPtr TifaMaxHPAddress = new IntPtr(0xDBA8EA); // 2 bytes
-        public static readonly IntPtr TifaCurHPAddress = new IntPtr(0xDBFEC0); // 2 bytes
+        public static readonly MemLoc CloudMaxHP = new MemLoc(0xDBFDBA, 2);
+        public static readonly MemLoc CloudCurHP = new MemLoc(0xDBFDB8, 2);
+        public static readonly MemLoc CloudStr = new MemLoc(0xDBFD8E);
+        public static readonly MemLoc CloudVit = new MemLoc(0xDBFD8F);
+        public static readonly MemLoc CloudMag = new MemLoc(0xDBFD90);
+        public static readonly MemLoc CloudSpi = new MemLoc(0xDBFD91);
+        public static readonly MemLoc CloudDex = new MemLoc(0xDBFD92);
+        public static readonly MemLoc CloudLuc = new MemLoc(0xDBFD93);
 
-        public static readonly IntPtr BarretMaxHPAddress = new IntPtr(0xDBAD2A); // 2 bytes
-        public static readonly IntPtr BarretCurHPAddress = new IntPtr(0xDBFE3C); // 2 bytes
+        public static readonly MemLoc TifaMaxHP = new MemLoc(0xDBFEC2, 2);
+        public static readonly MemLoc TifaCurHP = new MemLoc(0xDBFEC0, 2);
+        public static readonly MemLoc TifaStr = new MemLoc(0xDBFE96);
+        public static readonly MemLoc TifaVit = new MemLoc(0xDBFE97);
+        public static readonly MemLoc TifaMag = new MemLoc(0xDBFE98);
+        public static readonly MemLoc TifaSpi = new MemLoc(0xDBFE99);
+        public static readonly MemLoc TifaDex = new MemLoc(0xDBFE9A);
+        public static readonly MemLoc TifaLuc = new MemLoc(0xDBFE9B);
 
-        public static readonly IntPtr GilAddress = new IntPtr(0xDC08B4); // 4 bytes
+        public static readonly MemLoc BarretMaxHP = new MemLoc(0xDBFE3E, 2);
+        public static readonly MemLoc BarretCurHP = new MemLoc(0xDBFE3C, 2);
+        public static readonly MemLoc BarretStr = new MemLoc(0xDBFE12);
+        public static readonly MemLoc BarretVit = new MemLoc(0xDBFE13);
+        public static readonly MemLoc BarretMag = new MemLoc(0xDBFE14);
+        public static readonly MemLoc BarretSpi = new MemLoc(0xDBFE15);
+        public static readonly MemLoc BarretDex = new MemLoc(0xDBFE16);
+        public static readonly MemLoc BarretLuc = new MemLoc(0xDBFE17);
+
+        public static readonly MemLoc Gil = new MemLoc(0xDC08B4, 4); // 4 bytes
     }
 }
--- src/InteractiveSeven.UI/Memory/MenuColorAccessor.cs
+++ src/InteractiveSeven.UI/Memory/MenuColorAccessor.cs
@@ -19,10 +19,10 @@ namespace InteractiveSeven.UI.Memory
             byte[] topRightBuffer = new byte[3];
             byte[] botRightBuffer = new byte[3];
 
-            _memoryAccessor.ReadMem(processName, MemoryLocations.TopLeftAddress, topLeftBuffer);
-            _memoryAccessor.ReadMem(processName, MemoryLocations.BotLeftAddress, botLeftBuffer);
-            _memoryAccessor.ReadMem(processName, MemoryLocations.TopRightAddress, topRightBuffer);
-            _memoryAccessor.ReadMem(processName, MemoryLocations.BotRightAddress, botRightBuffer);
+            _memoryAccessor.ReadMem(processName, MemLoc.MenuTopLeft.Address, topLeftBuffer);
+            _memoryAccessor.ReadMem(processName, MemLoc.MenuBotLeft.Address, botLeftBuffer);
+            _memoryAccessor.ReadMem(processName, MemLoc.MenuTopRight.Address, topRightBuffer);
+            _memoryAccessor.ReadMem(processName, MemLoc.MenuBotRight.Address, botRightBuffer);
 
             return new MenuColors
             {
@@ -35,10 +35,10 @@ namespace InteractiveSeven.UI.Memory
 
         public void SetMenuColors(string processName, MenuColors menuColors)
         {
-            _memoryAccessor.WriteMem(processName, MemoryLocations.TopLeftAddress, menuColors.TopLeft.AsArray());
-            _memoryAccessor.WriteMem(processName, MemoryLocations.BotLeftAddress, menuColors.BotLeft.AsArray());
-            _memoryAccessor.WriteMem(processName, MemoryLocations.TopRightAddress, menuColors.TopRight.AsArray());
-            _memoryAccessor.WriteMem(processName, MemoryLocations.BotRightAddress, menuColors.BotRight.AsArray());
+            _memoryAccessor.WriteMem(processName, MemLoc.MenuTopLeft.Address, menuColors.TopLeft.AsArray());
+            _memoryAccessor.WriteMem(processName, MemLoc.MenuBotLeft.Address, menuColors.BotLeft.AsArray());
+            _memoryAccessor.WriteMem(processName, MemLoc.MenuTopRight.Address, menuColors.TopRight.AsArray());
+            _memoryAccessor.WriteMem(processName, MemLoc.MenuBotRight.Address, menuColors.BotRight.AsArray());
         }
 
     }
