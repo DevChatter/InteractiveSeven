5a5130088b3e8355f9c8721083db9a14fe04a8dd
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-18 00:56:27 -0400

    Making basic placeholder WPF App

---
+++ src/InteractiveSeven/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace InteractiveSeven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
