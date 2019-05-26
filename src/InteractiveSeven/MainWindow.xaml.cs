e6af57e9c47509e6c132d80385fc8beed8327460
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-26 18:43:14 -0400

    Adding WPF and some logo image work

---
+++ src/InteractiveSeven/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using InteractiveSeven.ViewModels;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace InteractiveSeven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MenuColorViewModel menuColorViewModel = new MenuColorViewModel();
+            MenuColorGrid.DataContext = menuColorViewModel;
+            ColorPreview.DataContext = menuColorViewModel;
+        }
+
+        private void PatreonLink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+            e.Handled = true;
+        }
+    }
+
+    public class ColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(color);
+            }
+            throw new ArgumentException($"Argument must be a {typeof(Color).FullName}", nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/InteractiveSeven/ViewModels/MenuColorViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using System.Linq;
+
+namespace InteractiveSeven.ViewModels
+{
+    public class MenuColorViewModel : INotifyPropertyChanged
+    {
+        private Color topLeft = Color.FromRgb(0, 88, 176);
+        private Color botLeft = Color.FromRgb(0, 0, 80);
+        private Color topRight = Color.FromRgb(0, 0, 128);
+        private Color botRight = Color.FromRgb(0, 0, 32);
+
+        public Color TopLeft
+        {
+            get => topLeft;
+            set
+            {
+                topLeft = value;
+                OnPropertyChanged();
+                OnPropertyChanged("MidPoint");
+            }
+        }
+        public Color BotLeft
+        {
+            get => botLeft;
+            set
+            {
+                botLeft = value;
+                OnPropertyChanged();
+                OnPropertyChanged("MidPoint");
+            }
+        }
+        public Color TopRight
+        {
+            get => topRight;
+            set
+            {
+                topRight = value;
+                OnPropertyChanged();
+                OnPropertyChanged("MidPoint");
+            }
+        }
+        public Color BotRight
+        {
+            get => botRight;
+            set
+            {
+                botRight = value;
+                OnPropertyChanged();
+                OnPropertyChanged("MidPoint");
+            }
+        }
+        public Color MidPoint
+        {
+            get => Color.FromRgb((byte)AverageRed(), (byte)AverageGreen(), (byte)AverageBlue());
+        }
+
+        private int AverageRed() => (int)(new int[] { topLeft.R, topRight.R, botLeft.R, botRight.R }).Average();
+        private int AverageGreen() => (int)(new int[] { topLeft.G, topRight.G, botLeft.G, botRight.G }).Average();
+        private int AverageBlue() => (int)(new int[] { topLeft.B, topRight.B, botLeft.B, botRight.B }).Average();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
