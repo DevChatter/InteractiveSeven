d970d26d718af77e0261a5167546f183c65e12fa
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-29 22:29:05 -0400

    Fix weird spacing

--- src/InteractiveSeven.Core/Settings/TwitchSettings.cs
+++ src/InteractiveSeven.Core/Settings/TwitchSettings.cs
@@ -3,9 +3,7 @@
     public class TwitchSettings
     {
         public string Username { get; set; }
-        public string AccessToken {
-            get;
-            set; }
+        public string AccessToken { get; set; }
         public string Channel { get; set; }
     }
 }
