532d9cf025590a7b39aba1056bb7fa847300168b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-12 15:27:29 -0400

    Fix naming in GilBank to prepare for more info

--- src/InteractiveSeven.Core/Models/GilBank.cs
+++ src/InteractiveSeven.Core/Models/GilBank.cs
@@ -5,33 +5,33 @@ namespace InteractiveSeven.Core.Models
 {
     public class GilBank
     {
-        private Dictionary<string, int> _ledger = new Dictionary<string, int>();
+        private Dictionary<string, int> Accounts { get; } = new Dictionary<string, int>();
 
-        private readonly object padlock = new object();
+        private readonly object _padlock = new object();
 
         public int Deposit(string username, int bits)
         {
-            lock (padlock)
+            lock (_padlock)
             {
-                _ledger.TryGetValue(username, out int balance);
+                Accounts.TryGetValue(username, out int balance);
                 balance += bits;
-                _ledger[username] = balance;
+                Accounts[username] = balance;
                 return balance;
             }
         }
 
         public (int balance, int withdrawn) Withdraw(string username, int bits, bool requireBalance = false)
         {
-            lock (padlock)
+            lock (_padlock)
             {
-                _ledger.TryGetValue(username, out int balance);
+                Accounts.TryGetValue(username, out int balance);
                 if (requireBalance && balance < bits)
                 {
                     return (balance, 0);
                 }
                 int withdrawn = Math.Min(balance, bits);
                 balance -= withdrawn;
-                _ledger[username] = balance;
+                Accounts[username] = balance;
                 return (balance, withdrawn);
             }
         }
