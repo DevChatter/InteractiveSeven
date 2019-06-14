1d375ed340a09d082e96e3637633eef49e7dd047
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-14 11:18:53 -0400

    Remove Duplicate CommandWords

--- src/InteractiveSeven.Core/Settings/CommandSettings.cs
+++ src/InteractiveSeven.Core/Settings/CommandSettings.cs
@@ -1,4 +1,9 @@
-﻿namespace InteractiveSeven.Core.Settings
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace InteractiveSeven.Core.Settings
 {
     public class CommandSettings : ObservableSettingsBase
     {
@@ -9,13 +14,45 @@
         private string[] _menuCommandWords = { "Menu", "MenuColor", "Window", "Windows" };
         private string[] _nameBidsCommandWords = { "NameBids" };
         private string[] _refreshCommandWords = { "Refresh" };
+        private List<(string Name, Func<string[]> Words)> AllWordSets { get; }
+
+        public CommandSettings()
+        {
+            AllWordSets = new List<(string,Func<string[]>)>
+            {
+                (nameof(CostsCommandWords),
+                    () => CostsCommandWords),
+                (nameof(BalanceCommandWords),
+                    () => BalanceCommandWords),
+                (nameof(GiveGilCommandWords),
+                    () => GiveGilCommandWords),
+                (nameof(I7CommandWords),
+                    () => I7CommandWords),
+                (nameof(MenuCommandWords),
+                    () => MenuCommandWords),
+                (nameof(NameBidsCommandWords),
+                    () => NameBidsCommandWords),
+                (nameof(RefreshCommandWords),
+                    () => RefreshCommandWords),
+            };
+        }
+
+        private string[] RemoveAllDuplicates(IEnumerable<string> strings, [CallerMemberName] string propertyName = null)
+        {
+            foreach (Func<string[]> wordSet in AllWordSets.Where(x => x.Name != propertyName).Select(x => x.Words))
+            {
+                strings = strings.Except(wordSet());
+            }
+
+            return strings.ToArray();
+        }
 
         public string[] CostsCommandWords
         {
             get => _costsCommandWords;
             set
             {
-                _costsCommandWords = value;
+                _costsCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -25,7 +62,7 @@
             get => _balanceCommandWords;
             set
             {
-                _balanceCommandWords = value;
+                _balanceCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -35,7 +72,7 @@
             get => _giveGilCommandWords;
             set
             {
-                _giveGilCommandWords = value;
+                _giveGilCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -45,7 +82,7 @@
             get => _i7CommandWords;
             set
             {
-                _i7CommandWords = value;
+                _i7CommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -55,7 +92,7 @@
             get => _menuCommandWords;
             set
             {
-                _menuCommandWords = value;
+                _menuCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -65,7 +102,7 @@
             get => _nameBidsCommandWords;
             set
             {
-                _nameBidsCommandWords = value;
+                _nameBidsCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
@@ -75,7 +112,7 @@
             get => _refreshCommandWords;
             set
             {
-                _refreshCommandWords = value;
+                _refreshCommandWords = RemoveAllDuplicates(value);
                 OnPropertyChanged();
             }
         }
