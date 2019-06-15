2d90b39b70a2caa6b743d1a337c42e30a4eb5d50
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-14 22:27:32 -0400

    With all the statuses

--- src/InteractiveSeven.Core/Battle/StatusEffects.cs
+++ src/InteractiveSeven.Core/Battle/StatusEffects.cs
@@ -20,6 +20,8 @@ namespace InteractiveSeven.Core.Battle
         public static StatusEffects ByWord(string word)
             => All.FirstOrDefault(x => x.Words.Any(w => w.EqualsIns(word)));
 
+        public static StatusEffects Dead = new StatusEffects(1);
+        public static StatusEffects NearDeath = new StatusEffects(2);
         public static StatusEffects Sleep = new StatusEffects(4, "sleep", "slp", "sleeping");
         public static StatusEffects Poison = new StatusEffects(8, "psn", "poison");
         public static StatusEffects Sadness = new StatusEffects(16, "sad", "sadness");
@@ -31,11 +33,19 @@ namespace InteractiveSeven.Core.Battle
         public static StatusEffects Stop = new StatusEffects(1024, "stop", "stopped");
         public static StatusEffects Frog = new StatusEffects(2048, "frog", "toad");
         public static StatusEffects Small = new StatusEffects(4096, "small");
+        public static StatusEffects SlowNumb = new StatusEffects(8192);
+        public static StatusEffects Petrify = new StatusEffects(16384);
         public static StatusEffects Regen = new StatusEffects(32768, "regen");
         public static StatusEffects Barrier = new StatusEffects(65536, "barrier");
         public static StatusEffects MBarrier = new StatusEffects(131072, "mbarrier");
         public static StatusEffects Reflect = new StatusEffects(262144, "reflect");
+        public static StatusEffects Dual = new StatusEffects(524288);
+        public static StatusEffects Shield = new StatusEffects(1048576);
+        public static StatusEffects DeathSentence = new StatusEffects(2097152);
+        public static StatusEffects Manipulate = new StatusEffects(4194304);
         public static StatusEffects Berserk = new StatusEffects(8388608, "berserk");
+        public static StatusEffects Peerless = new StatusEffects(16777216);
+        public static StatusEffects Paralyze = new StatusEffects(33554432);
         public static StatusEffects Darkness = new StatusEffects(67108864, "darkness");
 
     }
