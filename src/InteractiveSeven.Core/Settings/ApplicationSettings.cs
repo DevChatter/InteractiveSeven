53b76ee5e5503332153ba652be0a43326d6bfff2
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-15 11:07:43 -0400

    Adding Status Effect Settings

--- src/InteractiveSeven.Core/Settings/ApplicationSettings.cs
+++ src/InteractiveSeven.Core/Settings/ApplicationSettings.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace InteractiveSeven.Core.Settings
 {
@@ -59,6 +60,7 @@ namespace InteractiveSeven.Core.Settings
         public TwitchSettings TwitchSettings { get; set; } = new TwitchSettings();
         public MenuColorSettings MenuSettings { get; set; } = new MenuColorSettings();
         public NameBiddingSettings NameBiddingSettings { get; set; } = new NameBiddingSettings();
+        public BattleSettings BattleSettings { get; set; } = new BattleSettings();
 
         public static void LoadFromJson(string json)
         {
@@ -72,5 +74,53 @@ namespace InteractiveSeven.Core.Settings
                 // gulp
             }
         }
+    }
+
+    public class BattleSettings : ObservableSettingsBase
+    {
+        public BattleSettings()
+        {
+            StatusEffects = new[]
+            {
+                Sleep,
+                Poison,
+                Sadness,
+                Fury,
+                Confusion,
+                Silence,
+                Haste,
+                Slow,
+                Stop,
+                Frog,
+                Small,
+                Regen,
+                Barrier,
+                MBarrier,
+                Reflect,
+                Berserk,
+                Darkness,
+            };
+        }
+
+        [JsonIgnore]
+        public StatusEffectSettings[] StatusEffects { get; set; }
+
+        public StatusEffectSettings Sleep { get; set; } = new StatusEffectSettings{ Name = nameof(Sleep) };
+        public StatusEffectSettings Poison { get; set; } = new StatusEffectSettings{ Name = nameof(Poison)};
+        public StatusEffectSettings Sadness { get; set; } = new StatusEffectSettings{ Name = nameof(Sadness)};
+        public StatusEffectSettings Fury { get; set; } = new StatusEffectSettings{ Name = nameof(Fury)};
+        public StatusEffectSettings Confusion { get; set; } = new StatusEffectSettings{ Name = nameof(Confusion)};
+        public StatusEffectSettings Silence { get; set; } = new StatusEffectSettings{ Name = nameof(Silence)};
+        public StatusEffectSettings Haste { get; set; } = new StatusEffectSettings{ Name = nameof(Haste)};
+        public StatusEffectSettings Slow { get; set; } = new StatusEffectSettings{ Name = nameof(Slow)};
+        public StatusEffectSettings Stop { get; set; } = new StatusEffectSettings{ Name = nameof(Stop)};
+        public StatusEffectSettings Frog { get; set; } = new StatusEffectSettings{ Name = nameof(Frog)};
+        public StatusEffectSettings Small { get; set; } = new StatusEffectSettings{ Name = nameof(Small)};
+        public StatusEffectSettings Regen { get; set; } = new StatusEffectSettings{ Name = nameof(Regen)};
+        public StatusEffectSettings Barrier { get; set; } = new StatusEffectSettings{ Name = nameof(Barrier)};
+        public StatusEffectSettings MBarrier { get; set; } = new StatusEffectSettings{ Name = nameof(MBarrier)};
+        public StatusEffectSettings Reflect { get; set; } = new StatusEffectSettings{ Name = nameof(Reflect)};
+        public StatusEffectSettings Berserk { get; set; } = new StatusEffectSettings{ Name = nameof(Berserk)};
+        public StatusEffectSettings Darkness { get; set; } = new StatusEffectSettings{ Name = nameof(Darkness)};
     }
 }
---
+++ src/InteractiveSeven.Core/Settings/StatusEffectSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace InteractiveSeven.Core.Settings
+{
+    public class StatusEffectSettings : ObservableSettingsBase
+    {
+        public string Name { get; set; }
+
+        private bool _enabled = true;
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                _enabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _cost = 100;
+        public int Cost
+        {
+            get => _cost;
+            set
+            {
+                _cost = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
