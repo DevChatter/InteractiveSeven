119feffb3fff8a0a9f9836616926de2da76b0b0e
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-16 22:18:14 -0400

    Put StatusEffectSettings into collection only

--- src/InteractiveSeven.Core/Settings/BattleSettings.cs
+++ src/InteractiveSeven.Core/Settings/BattleSettings.cs
@@ -11,29 +11,29 @@ namespace InteractiveSeven.Core.Settings
         {
             AllStatusEffects = new[]
             {
-                Barrier,
-                Berserk,
-                Confusion,
-                Darkness,
-                DeathSentence,
-                Dual,
-                Frog,
-                Fury,
-                Haste,
-                MBarrier,
-                Paralyze,
-                Peerless,
-                Petrify,
-                Poison,
-                Reflect,
-                Regen,
-                Sadness,
-                Silence,
-                Sleep,
-                Slow,
-                Small,
-                Stop,
-                Shield,
+                new StatusEffectSettings("Barrier", StatusEffects.Barrier, true, 200, "barrier"),
+                new StatusEffectSettings("Berserk", StatusEffects.Berserk, true, 200, "berserk"),
+                new StatusEffectSettings("Confusion", StatusEffects.Confusion, false, 200, "conf", "confusion"),
+                new StatusEffectSettings("Darkness", StatusEffects.Darkness, true, 100, "darkness"),
+                new StatusEffectSettings("DeathSentence", StatusEffects.DeathSentence, true, 200, "deathsentence"),
+                new StatusEffectSettings("Dual", StatusEffects.Dual, false, 1000, "dual"),
+                new StatusEffectSettings("Frog", StatusEffects.Frog, true, 200, "frog", "toad"),
+                new StatusEffectSettings("Fury", StatusEffects.Fury, true, 100, "fury", "hyper"),
+                new StatusEffectSettings("Haste", StatusEffects.Haste, true, 200, "haste", "hasted"),
+                new StatusEffectSettings("MBarrier", StatusEffects.MBarrier, true, 200, "mbarrier"),
+                new StatusEffectSettings("Paralyze", StatusEffects.Paralyze, false, 1000, "paralyze"),
+                new StatusEffectSettings("Peerless", StatusEffects.Peerless, false, 500, "peerless"),
+                new StatusEffectSettings("Petrify", StatusEffects.Petrify, false, 1000, "petrify"),
+                new StatusEffectSettings("Poison", StatusEffects.Poison, true, 100, "psn", "poison"),
+                new StatusEffectSettings("Reflect", StatusEffects.Reflect, true, 200, "reflect"),
+                new StatusEffectSettings("Regen", StatusEffects.Regen, true, 200, "regen"),
+                new StatusEffectSettings("Sadness", StatusEffects.Sadness, true, 100, "sad", "sadness"),
+                new StatusEffectSettings("Shield", StatusEffects.Shield, true, 200, "shield"),
+                new StatusEffectSettings("Silence", StatusEffects.Silence, true, 200, "sil", "silence"),
+                new StatusEffectSettings("Sleep", StatusEffects.Sleep, true, 100, "sleep", "slp", "sleeping"),
+                new StatusEffectSettings("Slow", StatusEffects.Slow, true, 100, "slow", "slowed"),
+                new StatusEffectSettings("Small", StatusEffects.Small, true, 200, "small"),
+                new StatusEffectSettings("Stop", StatusEffects.Stop, false, 1000, "stop", "stopped"),
             };
         }
 
@@ -50,53 +50,6 @@ namespace InteractiveSeven.Core.Settings
 
         [JsonIgnore]
         public StatusEffectSettings[] AllStatusEffects { get; set; }
-
-        public StatusEffectSettings Sleep { get; set; }
-            = new StatusEffectSettings(nameof(Sleep), StatusEffects.Sleep, true, 100, "sleep", "slp", "sleeping");
-        public StatusEffectSettings Poison { get; set; }
-            = new StatusEffectSettings(nameof(Poison), StatusEffects.Poison, true, 100, "psn", "poison");
-        public StatusEffectSettings Sadness { get; set; }
-            = new StatusEffectSettings(nameof(Sadness), StatusEffects.Sadness, true, 100, "sad", "sadness");
-        public StatusEffectSettings Fury { get; set; }
-            = new StatusEffectSettings(nameof(Fury), StatusEffects.Fury, true, 100, "fury", "hyper");
-        public StatusEffectSettings Confusion { get; set; }
-            = new StatusEffectSettings(nameof(Confusion), StatusEffects.Confusion, false, 200, "conf", "confusion");
-        public StatusEffectSettings Silence { get; set; }
-            = new StatusEffectSettings(nameof(Silence), StatusEffects.Silence, true, 200, "sil", "silence");
-        public StatusEffectSettings Haste { get; set; }
-            = new StatusEffectSettings(nameof(Haste), StatusEffects.Haste, true, 200, "haste", "hasted");
-        public StatusEffectSettings Slow { get; set; }
-            = new StatusEffectSettings(nameof(Slow), StatusEffects.Slow, true, 100, "slow", "slowed");
-        public StatusEffectSettings Stop { get; set; }
-            = new StatusEffectSettings(nameof(Stop), StatusEffects.Stop, false, 1000, "stop", "stopped");
-        public StatusEffectSettings Frog { get; set; }
-            = new StatusEffectSettings(nameof(Frog), StatusEffects.Frog, true, 200, "frog", "toad");
-        public StatusEffectSettings Small { get; set; }
-            = new StatusEffectSettings(nameof(Small), StatusEffects.Small, true, 200, "small");
-        public StatusEffectSettings Regen { get; set; }
-            = new StatusEffectSettings(nameof(Regen), StatusEffects.Regen, true, 200, "regen");
-        public StatusEffectSettings Barrier { get; set; }
-            = new StatusEffectSettings(nameof(Barrier), StatusEffects.Barrier, true, 200, "barrier");
-        public StatusEffectSettings MBarrier { get; set; }
-            = new StatusEffectSettings(nameof(MBarrier), StatusEffects.MBarrier, true, 200, "mbarrier");
-        public StatusEffectSettings Reflect { get; set; }
-            = new StatusEffectSettings(nameof(Reflect), StatusEffects.Reflect, true, 200, "reflect");
-        public StatusEffectSettings Berserk { get; set; }
-            = new StatusEffectSettings(nameof(Berserk), StatusEffects.Berserk, true, 200, "berserk");
-        public StatusEffectSettings Darkness { get; set; }
-            = new StatusEffectSettings(nameof(Darkness), StatusEffects.Darkness, true, 100, "darkness");
-        public StatusEffectSettings Petrify { get; set; }
-            = new StatusEffectSettings(nameof(Petrify), StatusEffects.Petrify, false, 1000, "petrify");
-        public StatusEffectSettings Dual { get; set; }
-            = new StatusEffectSettings(nameof(Dual), StatusEffects.Dual, false, 1000, "dual");
-        public StatusEffectSettings Shield { get; set; }
-            = new StatusEffectSettings(nameof(Shield), StatusEffects.Shield, true, 200, "shield");
-        public StatusEffectSettings DeathSentence { get; set; }
-            = new StatusEffectSettings(nameof(DeathSentence), StatusEffects.DeathSentence, true, 200, "deathsentence");
-        public StatusEffectSettings Peerless { get; set; }
-            = new StatusEffectSettings(nameof(Peerless), StatusEffects.Peerless, false, 500, "peerless");
-        public StatusEffectSettings Paralyze { get; set; }
-            = new StatusEffectSettings(nameof(Paralyze), StatusEffects.Paralyze, false, 1000, "paralyze");
 
         public StatusEffectSettings ByWord(string word)
         {
