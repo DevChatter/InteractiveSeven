0ec1699c4e184bcbd8e64199af57e528f6991f16
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-06-21 21:53:29 -0400

    Fix an off-by-one

--- src/InteractiveSeven.Core/Memory/InventorySlot.cs
+++ src/InteractiveSeven.Core/Memory/InventorySlot.cs
@@ -14,7 +14,7 @@
         public InventorySlot(byte[] bytes)
         {
             Quantity = (ushort)(bytes[1] / 2);
-            ItemId = (ushort)(bytes[0] + (bytes[1] % 2 == 0 ? 0 : 255));
+            ItemId = (ushort)(bytes[0] + (bytes[1] % 2 == 0 ? 0 : 256));
         }
 
         public byte[] AsBytes
@@ -22,7 +22,7 @@
             get
             {
                 var bytes = new byte[2];
-                bytes[0] = (byte) (ItemId > 255 ? ItemId - 255 : ItemId);
+                bytes[0] = (byte) (ItemId > 255 ? ItemId - 256 : ItemId);
                 bytes[1] = (byte) (Quantity * 2 + (ItemId > 255 ? 1 : 0));
                 return bytes;
             }
