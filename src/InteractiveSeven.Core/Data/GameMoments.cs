7594316c79db5e35ae866f992b73d597adacabef
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-07-03 13:39:48 -0400

    Adding Found GameMoments

---
+++ src/InteractiveSeven.Core/Data/GameMoments.cs
@@ -0,0 +1,42 @@
+﻿namespace InteractiveSeven.Core.Data
+{
+    public enum GameMoments : ushort
+    {
+        OpeningMovie = 0,
+        BombingRun = 1,
+        FirstBattle = 6,
+        MeetingAvalanche = 7, // Cloud and Barret get names
+        AfterBarretNamed = 8,
+        FirstReactorBridgeIn = 10,
+        BiggsJessieOpenEntrance = 11,
+        FirstEelvatorDown = 12,
+        FirstBattleAfterElevator = 13,
+        JessieTaughtLadders = 14,
+        ReceivedRestoreMateria = 15,
+        AfterGuardScorpion = 27,
+        FirstReactorExploding = 33,
+        AfterFirstReactor = 36,
+        AfterFlowerGirl = 39,
+        AfterTrainJumpAndConvo = 48,
+        JessieMapLesson = 51,
+        ExitTrainHuddle = 63,
+        TifaNamed = 69,
+        BarWithTifa = 72,
+        AfterMoneyArgument = 78,
+        PromiseFlashback = 84,
+        AfterPromiseFlashback = 87,
+        ReceiveFirstPayment = 81,
+        WakeForSecondMission = 105,
+        SecurityAlertTrain = 108,
+        IntoSecondReactor = 117,
+        SecondReactorFlashback = 120,
+        AfterSecondReactorFlashback = 123,
+        AfterSecondBombPlaced = 127,
+        AfterButtonPressGame = 128,
+        CloudFallSceneAfterAirbuster = 140,
+        EscapeChurchWithAeris = 149,
+        BarrelMinigame = 152,
+        ChurchRooftop = 155,
+        JumpingRooftopsWithAeris = 158,
+    }
+}
