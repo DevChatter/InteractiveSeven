ae5572439bbf31f5a2eee4746464dd4ef8b9e36d
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-07-03 20:52:46 -0400

    Default GameMoments for charname after

--- src/InteractiveSeven.Core/Data/CharNames.cs
+++ src/InteractiveSeven.Core/Data/CharNames.cs
@@ -14,7 +14,8 @@ namespace InteractiveSeven.Core.Data
 
         public GameMoments AllowNamingAfter { get; }
 
-        private CharNames(int id, string defaultName, Func<CommandSettings, IList<string>> wordsSelector, GameMoments allowNamingAfter)
+        private CharNames(int id, string defaultName, Func<CommandSettings, IList<string>> wordsSelector,
+            GameMoments allowNamingAfter = GameMoments.AfterBarretNamed)
         {
             _wordsSelector = wordsSelector;
             AllowNamingAfter = allowNamingAfter;
@@ -39,14 +40,14 @@ namespace InteractiveSeven.Core.Data
             return (charName != null, charName);
         }
 
-        public static CharNames Cloud = new CharNames(1, "Cloud", x => x.CloudCommandWords, GameMoments.AfterBarretNamed);
-        public static CharNames Tifa = new CharNames(2, "Tifa", x => x.TifaCommandWords, GameMoments.AfterBarretNamed);
-        public static CharNames Barret = new CharNames(3, "Barret", x => x.BarretCommandWords, GameMoments.AfterBarretNamed);
-        public static CharNames Aeris = new CharNames(4, "Aeris", x => x.AerisCommandWords, GameMoments.AfterBarretNamed);
-        public static CharNames Red = new CharNames(5, "Red XIII", x => x.RedCommandWords, GameMoments.AfterBarretNamed);
+        public static CharNames Cloud = new CharNames(1, "Cloud", x => x.CloudCommandWords);
+        public static CharNames Tifa = new CharNames(2, "Tifa", x => x.TifaCommandWords);
+        public static CharNames Barret = new CharNames(3, "Barret", x => x.BarretCommandWords);
+        public static CharNames Aeris = new CharNames(4, "Aeris", x => x.AerisCommandWords);
+        public static CharNames Red = new CharNames(5, "Red XIII", x => x.RedCommandWords);
         public static CharNames CaitSith = new CharNames(6, "Cait Sith", x => x.CaitCommandWords, GameMoments.AfterKalmFlashback);
         public static CharNames Vincent = new CharNames(7, "Vincent", x => x.VincentCommandWords, GameMoments.AfterKalmFlashback);
-        public static CharNames Yuffie = new CharNames(8, "Yuffie", x => x.YuffieCommandWords, GameMoments.AfterBarretNamed);
-        public static CharNames Cid = new CharNames(9, "Cid", x => x.CidCommandWords, GameMoments.AfterBarretNamed);
+        public static CharNames Yuffie = new CharNames(8, "Yuffie", x => x.YuffieCommandWords);
+        public static CharNames Cid = new CharNames(9, "Cid", x => x.CidCommandWords);
     }
 }
