551bdc3cd066d12ce4511c5840ce7d9ceb948cf0
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-07-16 15:46:34 -0400

    Adding a Menu View for displaying blank menus

---
+++ src/InteractiveSeven.Web/Controllers/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveSeven.Web.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
