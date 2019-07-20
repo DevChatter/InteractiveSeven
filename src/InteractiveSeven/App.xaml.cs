c8f7980e33836a8c52840b92b538c46e98a4aea4
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-07-20 15:23:27 -0400

    Extract Dependency Registration

--- src/InteractiveSeven/App.xaml.cs
+++ src/InteractiveSeven/App.xaml.cs
@@ -1,30 +1,12 @@
-﻿using InteractiveSeven.Core;
-using InteractiveSeven.Core.Data;
-using InteractiveSeven.Core.IntervalMessages;
-using InteractiveSeven.Core.Memory;
-using InteractiveSeven.Core.Models;
-using InteractiveSeven.Core.Payments;
-using InteractiveSeven.Core.Services;
-using InteractiveSeven.Core.Settings;
-using InteractiveSeven.Core.ViewModels;
+﻿using InteractiveSeven.Core.Settings;
 using InteractiveSeven.Core.Workloads;
-using InteractiveSeven.Services;
 using InteractiveSeven.Startup;
-using InteractiveSeven.Twitch;
-using InteractiveSeven.Twitch.Commands;
-using InteractiveSeven.Twitch.IntervalMessages;
-using InteractiveSeven.Twitch.Payments;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using System.Collections.Generic;
 using System.Windows;
-using InteractiveSeven.Core.Emitters;
-using InteractiveSeven.Web.Hubs;
 using Tseng;
-using TwitchLib.Client;
-using TwitchLib.Client.Interfaces;
 
 namespace InteractiveSeven
 {
@@ -58,7 +40,7 @@ namespace InteractiveSeven
                     webHostBuilder.UseStartup<InteractiveSeven.Web.Startup>();
                     //webHostBuilder.UseContentRoot()
                 })
-                .ConfigureServices(ConfigureServices)
+                .ConfigureServices(DependencyRegistrar.ConfigureServices)
                 .Build();
 
             _host.Start();
@@ -72,80 +54,6 @@ namespace InteractiveSeven
             _tsengProgram.Start();
 
             _host.Services.GetRequiredService<MainWindow>().Show();
-        }
-
-        private void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient(typeof(IList<>), typeof(List<>));
-
-            services.AddSingleton<WorkloadCoordinator>();
-
-            services.AddSingleton<MenuColorViewModel>();
-            services.AddSingleton<NameBiddingViewModel>();
-            services.AddSingleton<SettingsViewModel>();
-            services.AddSingleton<MainWindowViewModel>();
-
-            services.AddSingleton<PartyStatusViewModel>();
-
-            services.AddSingleton<IMenuHubEmitter, MenuHubEmitter>();
-
-            services.AddSingleton<IClock, SystemClock>();
-            services.AddSingleton<IIntervalMessagingService, IntervalMessagingService>();
-            services.AddSingleton<IEquipmentAccessor, EquipmentAccessor>();
-            services.AddSingleton<IMemoryAccessor, MemoryAccessor>();
-            services.AddSingleton<IGameMomentAccessor, GameMomentAccessor>();
-            services.AddSingleton<IMenuColorAccessor, MenuColorAccessor>();
-            services.AddSingleton<IGilAccessor, GilAccessor>();
-            services.AddSingleton<IInventoryAccessor, InventoryAccessor>();
-            services.AddSingleton<IMateriaAccessor, MateriaAccessor>();
-            services.AddSingleton<INameAccessor, NameAccessor>();
-            services.AddSingleton<IStatusAccessor, StatusAccessor>();
-            services.AddSingleton<ITwitchClient, TwitchClient>();
-            services.AddSingleton<IDialogService, DialogService>();
-
-            services.RegisterEquipmentData();
-
-            services.RegisterBattleCommand<StatusEffectCommand>();
-
-            services.RegisterNonBattleCommand<WeaponCommand>();
-            services.RegisterNonBattleCommand<ArmletCommand>();
-            services.RegisterNonBattleCommand<AccessoryCommand>();
-            services.RegisterNonBattleCommand<PauperCommand>();
-
-            services.RegisterTwitchCommand<PaletteCommand>();
-            services.RegisterTwitchCommand<RainbowCommand>();
-            services.RegisterTwitchCommand<MakoCommand>();
-            services.RegisterTwitchCommand<ItemCommand>();
-            services.RegisterTwitchCommand<MateriaCommand>();
-            services.RegisterTwitchCommand<CostsCommand>();
-            services.RegisterTwitchCommand<GiveGilCommand>();
-            services.RegisterTwitchCommand<NameBidsCommand>();
-            services.RegisterTwitchCommand<MenuCommand>();
-            services.RegisterTwitchCommand<NameCommand>();
-            services.RegisterTwitchCommand<RefreshCommand>();
-            services.RegisterTwitchCommand<BalanceCommand>();
-            services.RegisterTwitchCommand<HelpCommand>();
-            services.RegisterTwitchCommand<I7Command>();
-
-            services.AddSingleton<IChatBot, ChatBot>();
-            services.AddSingleton<TsengProgram>();
-
-            services.AddSingleton(typeof(IDataStore<>), typeof(FileDataStore<>));
-
-            services.AddSingleton<DataLoader>();
-
-            services.AddSingleton<ISettingsStore, SettingsStore>();
-            services.AddSingleton<GilBank>();
-            services.AddSingleton<PaymentProcessor>();
-            services.AddSingleton<ColorPaletteCollection>();
-            services.AddSingleton<MainWindow>();
-
-            services.AddLogging(config =>
-            {
-                config.AddSerilog();
-            });
-
-            services.AddMvcCore();
         }
 
         private void App_OnExit(object sender, ExitEventArgs e)
