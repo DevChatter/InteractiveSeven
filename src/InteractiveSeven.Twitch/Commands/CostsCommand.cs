54039b4647949935e86bd8549699e564cef11c4b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-11-15 22:36:32 -0500

    More CostCommand info

--- src/InteractiveSeven.Twitch/Commands/CostsCommand.cs
+++ src/InteractiveSeven.Twitch/Commands/CostsCommand.cs
@@ -1,4 +1,6 @@
-﻿using InteractiveSeven.Twitch.Model;
+﻿using InteractiveSeven.Core;
+using InteractiveSeven.Twitch.Model;
+using System.Linq;
 using TwitchLib.Client.Interfaces;
 
 namespace InteractiveSeven.Twitch.Commands
@@ -15,24 +17,60 @@ namespace InteractiveSeven.Twitch.Commands
 
         public override void Execute(in CommandData commandData)
         {
+            string argument = commandData.Arguments.FirstOrDefault();
+            string message = "";
+            if (argument is null)
+            {
+                message = "Specify cost type to check (color, status).";
+            }
+            else if (argument.EqualsIns("color") || argument.EqualsIns("menu"))
+            {
+                message = GetColorCostMessage();
+            }
+            else if (argument.EqualsIns("status") || argument.EqualsIns("statuses"))
+            {
+                message = GetStatusCostMessage();
+            }
+
+            _twitchClient.SendMessage(commandData.Channel, message);
+        }
+
+        private string GetStatusCostMessage()
+        {
+            string message = "";
+
+            if (Settings.BattleSettings.AllowStatusEffects)
+            {
+                foreach (var effect in Settings.BattleSettings.AllStatusEffects.Where(x => x.Enabled))
+                {
+                    message += $"[{effect.Name}: {effect.Cost}] ";
+                }
+            }
+
+            return message;
+        }
+
+        private string GetColorCostMessage()
+        {
             string message = "";
 
             if (Settings.MenuSettings.Enabled)
             {
                 message += $"[Change Menu: {Settings.MenuSettings.BitCost}] ";
+
+                if (Settings.MenuSettings.EnableMakoCommand)
+                {
+                    message += $"[Mako Mode: {Settings.MenuSettings.MakoModeCost}] ";
+                }
+
+                if (Settings.MenuSettings.EnableRainbowCommand)
+                {
+                    message += $"[Rainbow Mode: {Settings.MenuSettings.RainbowModeCost}] ";
+                }
+
             }
 
-            if (Settings.MenuSettings.EnableMakoCommand)
-            {
-                message += $"[Mako Mode: {Settings.MenuSettings.MakoModeCost}] ";
-            }
-
-            if (Settings.MenuSettings.EnableRainbowCommand)
-            {
-                message += $"[Rainbow Mode: {Settings.MenuSettings.RainbowModeCost}] ";
-            }
-
-            _twitchClient.SendMessage(commandData.Channel, message);
+            return message;
         }
     }
 }
