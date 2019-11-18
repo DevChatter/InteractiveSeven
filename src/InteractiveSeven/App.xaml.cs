dd06f47d9b4219fb88d07cbdfc70597e2a59d3aa
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-11-17 22:48:33 -0500

    Adding more logging

--- src/InteractiveSeven/App.xaml.cs
+++ src/InteractiveSeven/App.xaml.cs
@@ -10,7 +10,9 @@ using Serilog;
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using Serilog.Extensions.Logging;
 using Tseng;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace InteractiveSeven
 {
@@ -25,46 +27,54 @@ namespace InteractiveSeven
         private TsengProgram _tsengProgram;
         private ILogger<App> _logger;
 
-        private static void InitializeSettings()
+        private static void InitializeSettings(ILogger logger)
         {
-            new SettingsStore().EnsureExists();
+            new SettingsStore().EnsureExists(ex => logger.LogError(ex, "Error loading settings from JSON."));
         }
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            InitializeSettings();
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .WriteTo.File("logs\\i7log.txt", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
 
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.File("logs\\i7log.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
+                var logger = new SerilogLoggerProvider(Log.Logger).CreateLogger(nameof(SettingsStore));
+                InitializeSettings(logger);
 
-            var uri = new UriBuilder("http", "localhost", ApplicationSettings.Instance.TsengSettings.PortNumber).Uri;
-            _host = WebHost.CreateDefaultBuilder(e.Args)
-                .UseStartup<InteractiveSeven.Web.Startup>()
-                .UseUrls(uri.AbsoluteUri)
-                .ConfigureServices(DependencyRegistrar.ConfigureServices)
-                .Build();
+                var uri = new UriBuilder("http", "localhost", ApplicationSettings.Instance.TsengSettings.PortNumber).Uri;
+                _host = WebHost.CreateDefaultBuilder(e.Args)
+                    .UseStartup<InteractiveSeven.Web.Startup>()
+                    .UseUrls(uri.AbsoluteUri)
+                    .ConfigureServices(DependencyRegistrar.ConfigureServices)
+                    .Build();
 
-            _logger = _host.Services.GetService<ILogger<App>>();
+                _logger = _host.Services.GetService<ILogger<App>>();
 
-            _logger.LogInformation("Starting Web Host...");
+                _logger.LogInformation("Starting Web Host...");
 
-            _host.Start();
+                _host.Start();
 
-            var dataLoader = _host.Services.GetService<DataLoader>();
-            _logger.LogInformation("Starting Elena DataLoader...");
-            dataLoader.LoadPreviousData();
+                var dataLoader = _host.Services.GetService<DataLoader>();
+                _logger.LogInformation("Starting Elena DataLoader...");
+                dataLoader.LoadPreviousData();
 
-            _workloadCoordinator = _host.Services.GetService<WorkloadCoordinator>();
+                _workloadCoordinator = _host.Services.GetService<WorkloadCoordinator>();
 
-            _tsengProgram = _host.Services.GetService<TsengProgram>();
+                _tsengProgram = _host.Services.GetService<TsengProgram>();
 
-            _logger.LogInformation("Starting Tseng Background Monitoring...");
-            Task.Run(() => _tsengProgram.Start()).RunInBackgroundSafely(false, LogTsengError);
+                _logger.LogInformation("Starting Tseng Background Monitoring...");
+                Task.Run(() => _tsengProgram.Start()).RunInBackgroundSafely(false, LogTsengError);
 
-            _logger.LogInformation("Showing App Main Window...");
-            _host.Services.GetRequiredService<MainWindow>().Show();
+                _logger.LogInformation("Showing App Main Window...");
+                _host.Services.GetRequiredService<MainWindow>().Show();
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception, "Error Loading Application");
+            }
         }
 
         private void LogTsengError(Exception ex)
