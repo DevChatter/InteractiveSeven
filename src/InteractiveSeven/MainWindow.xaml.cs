df52af1fde27150008f88e8686db503ae39417d5
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-03-04 09:43:10 -0500

    Adding MahApps

--- src/InteractiveSeven/MainWindow.xaml.cs
+++ src/InteractiveSeven/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using InteractiveSeven.Core.ViewModels;
+using MahApps.Metro.Controls;
 using System;
 using System.Diagnostics;
 using System.Windows;
@@ -10,7 +11,7 @@ namespace InteractiveSeven
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
 
         public MainWindow(MainWindowViewModel viewModel)
