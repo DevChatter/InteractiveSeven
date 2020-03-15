3bf03149a01ae3ccf4e13e124d9b09ab5c1ee6af
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-03-14 23:44:47 -0400

    Scene Mapping struct

---
+++ src/InteractiveSeven.Core/FinalFantasy/MemModels/Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace InteractiveSeven.Core.FinalFantasy.MemModels
+{
+    // Starting Address 0x009A8E9C
+    [StructLayout(LayoutKind.Explicit, Size = 184)]
+    public struct Scene
+    {
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = 12)]
+        [FieldOffset(0)] public byte[] RawName;
+    }
+}
