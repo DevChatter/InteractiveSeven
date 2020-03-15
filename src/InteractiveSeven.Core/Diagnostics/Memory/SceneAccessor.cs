220f9b55dfd9996ee412a5e9e499e08e87ba4801
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-03-15 14:45:21 -0400

    Adding initial SceneAccessor

---
+++ src/InteractiveSeven.Core/Diagnostics/Memory/SceneAccessor.cs
@@ -0,0 +1,37 @@
+﻿using InteractiveSeven.Core.FinalFantasy;
+using InteractiveSeven.Core.FinalFantasy.Constants;
+using InteractiveSeven.Core.FinalFantasy.MemModels;
+using InteractiveSeven.Core.Settings;
+using System.Linq;
+
+namespace InteractiveSeven.Core.Diagnostics.Memory
+{
+    public class SceneAccessor : ISceneAccessor
+    {
+        private readonly IMemoryAccessor _memoryAccessor;
+
+        private string ProcessName => ApplicationSettings.Instance.ProcessName;
+
+        public SceneAccessor(IMemoryAccessor memoryAccessor)
+        {
+            _memoryAccessor = memoryAccessor;
+        }
+
+        public Scene GetCurrentScene()
+        {
+            var buffer = new byte[Addresses.SceneMapStart.NumBytes];
+            _memoryAccessor.ReadMem(ProcessName, Addresses.SceneMapStart.Address, buffer);
+
+            string[] enemyNames = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+
+                var nameBytes = buffer.Skip(SceneMapConst.ActorSize * i + SceneMapConst.EnemyNameOffset)
+                    .TakeWhile(x => x != SceneMapConst.EnemyNameEnd).ToArray();
+                enemyNames[i] = nameBytes.MapFf7BytesToString(nameBytes.Length);
+            }
+
+            return new Scene(enemyNames);
+        }
+    }
+}
