d9edbd98781b9bfdee7092195998e4cad8ff6dbc
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-03-15 23:58:30 -0400

    Add lock/unlock mechanism

---
+++ src/InteractiveSeven.Core/Diagnostics/Memory/IMenuStatusAccessor.cs
@@ -0,0 +1,11 @@
+﻿using InteractiveSeven.Core.FinalFantasy;
+
+namespace InteractiveSeven.Core.Diagnostics.Memory
+{
+    public interface IMenuStatusAccessor
+    {
+        void LockMenu(MenuFlags menuFlags);
+        void UnlockMenu(MenuFlags menuFlags);
+        MenuFlags GetMenuStatus();
+    }
+}
---
+++ src/InteractiveSeven.Core/Diagnostics/Memory/MenuStatusAccessor.cs
@@ -0,0 +1,47 @@
+﻿using InteractiveSeven.Core.FinalFantasy;
+using InteractiveSeven.Core.Settings;
+using System;
+
+namespace InteractiveSeven.Core.Diagnostics.Memory
+{
+    public class MenuStatusAccessor : IMenuStatusAccessor
+    {
+        private readonly IMemoryAccessor _memoryAccessor;
+
+        private string ProcessName => ApplicationSettings.Instance.ProcessName;
+
+        public MenuStatusAccessor(IMemoryAccessor memoryAccessor)
+        {
+            _memoryAccessor = memoryAccessor;
+        }
+
+        public void LockMenu(MenuFlags menuFlags)
+        {
+            MenuFlags menuStatus = GetMenuStatus();
+
+            menuStatus |= menuFlags;
+
+            byte[] bytes = BitConverter.GetBytes((int)menuStatus);
+
+            _memoryAccessor.WriteMem(ProcessName, Addresses.MenuLock.Address, bytes);
+        }
+
+        public void UnlockMenu(MenuFlags menuFlags)
+        {
+            MenuFlags menuStatus = GetMenuStatus();
+
+            menuStatus ^= menuFlags;
+
+            byte[] bytes = BitConverter.GetBytes((int)menuStatus);
+
+            _memoryAccessor.WriteMem(ProcessName, Addresses.MenuLock.Address, bytes);
+        }
+
+        public MenuFlags GetMenuStatus()
+        {
+            byte[] bytes = new byte[Addresses.MenuLock.NumBytes];
+            _memoryAccessor.ReadMem(ProcessName, Addresses.MenuLock.Address, bytes);
+            return (MenuFlags)BitConverter.ToUInt16(bytes);
+        }
+    }
+}
