58e5b94fbe2e85b25fc0a07683bde4e587a29ee2
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-02 15:17:50 -0400

    Menu Color Preview; About Tab Styling

---
+++ src/InteractiveSeven/ValueConverters/ColorToSolidBrushValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace InteractiveSeven.ValueConverters
+{
+    public class ColorToSolidBrushValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is System.Drawing.Color drawColor)
+            {
+                return new SolidColorBrush(Color.FromRgb(drawColor.R, drawColor.G, drawColor.B));
+            }
+            return new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
