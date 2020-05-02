67f9f6e0a3a84fec9ecbe55257c3002b9c9b6a1b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-02 16:27:37 -0400

    Temp status display of settings

---
+++ src/InteractiveSeven/ValueConverters/BoolToOnOffConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InteractiveSeven.ValueConverters
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public sealed class BoolToOnOffConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+            return (bool)value ? "On" : "Off";
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
