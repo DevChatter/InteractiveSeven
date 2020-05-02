c7b7bfa30f38bf9307792a2f3f7a9fc395f7f56c
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-02 16:07:40 -0400

    Select All Textbox behavior for overlays

---
+++ src/InteractiveSeven/Behaviors/SelectAllTextOnFocusBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace InteractiveSeven.Behaviors
+{
+    public class SelectAllTextOnFocusBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.GotKeyboardFocus += AssociatedObjectGotKeyboardFocus;
+            AssociatedObject.GotMouseCapture += AssociatedObjectGotMouseCapture;
+            AssociatedObject.PreviewMouseLeftButtonDown += AssociatedObjectPreviewMouseLeftButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.GotKeyboardFocus -= AssociatedObjectGotKeyboardFocus;
+            AssociatedObject.GotMouseCapture -= AssociatedObjectGotMouseCapture;
+            AssociatedObject.PreviewMouseLeftButtonDown -= AssociatedObjectPreviewMouseLeftButtonDown;
+        }
+
+        private void AssociatedObjectGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+
+        private void AssociatedObjectGotMouseCapture(object sender, MouseEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+
+        private void AssociatedObjectPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!AssociatedObject.IsKeyboardFocusWithin)
+            {
+                AssociatedObject.Focus();
+                e.Handled = true;
+            }
+        }
+    }
+}
