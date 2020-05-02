8795d2774b10d3369828c9687e2f958deea58b43
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-02 15:47:12 -0400

    Color name display on dashboard

---
+++ src/InteractiveSeven/ValueConverters/MenuColorNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InteractiveSeven.ValueConverters
+{
+    public class MenuColorNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is System.Drawing.Color color)
+            {
+                string colorName = color.IsNamedColor ? color.Name : $"#{color.Name[2..]}";
+                return $"{colorName} | R:{color.R} G:{color.G} B:{color.B}";
+            }
+            return "Unexpected Binding";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
