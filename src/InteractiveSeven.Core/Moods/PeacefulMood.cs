b163868cfee4acd8d57a20b16523d42d4effd03f
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-23 16:40:54 -0400

    Move Moods to own files

---
+++ src/InteractiveSeven.Core/Moods/PeacefulMood.cs
@@ -0,0 +1,47 @@
+﻿using InteractiveSeven.Core.Battle;
+using InteractiveSeven.Core.Diagnostics.Memory;
+using InteractiveSeven.Core.ViewModels;
+
+namespace InteractiveSeven.Core.Moods
+{
+    public class PeacefulMood : BaseMood
+    {
+        private readonly IStatusAccessor _statusAccessor;
+        private readonly PartyStatusViewModel _partyStatus;
+        private readonly IBattleInfoAccessor _battleInfoAccessor;
+
+        public PeacefulMood(IStatusAccessor statusAccessor, PartyStatusViewModel partyStatus,
+            IBattleInfoAccessor battleInfoAccessor) : base("Peaceful Mood")
+        {
+            _statusAccessor = statusAccessor;
+            _partyStatus = partyStatus;
+            _battleInfoAccessor = battleInfoAccessor;
+        }
+
+        public override int Id => 2;
+
+        public override void ApplyEffect()
+        {
+            if (IsBattleActive())
+            {
+                var (valid, _, _) = _partyStatus.CheckTargetValidity(Allies.All, StatusEffects.Barrier);
+                foreach (Allies target in valid)
+                {
+                    _statusAccessor.SetActorStatus(target, StatusEffects.Barrier);
+                }
+                (valid, _, _) = _partyStatus.CheckTargetValidity(Allies.All, StatusEffects.MBarrier);
+                foreach (Allies target in valid)
+                {
+                    _statusAccessor.SetActorStatus(target, StatusEffects.MBarrier);
+                }
+            }
+        }
+
+        protected bool IsBattleActive()
+        {
+            var ff7BattleMap = _battleInfoAccessor.GetBattleMap();
+            return ff7BattleMap.IsActiveBattle && !ff7BattleMap.IsBattleEnding;
+        }
+
+    }
+}
