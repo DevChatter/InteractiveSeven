bfb71de18ae07da816ccbb238e4e5f93e89d2b97
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-05-23 16:40:54 -0400

    Move Moods to own files

---
+++ src/InteractiveSeven.Core/Moods/DangerMood.cs
@@ -0,0 +1,45 @@
+﻿using InteractiveSeven.Core.Battle;
+using InteractiveSeven.Core.Diagnostics.Memory;
+using InteractiveSeven.Core.ViewModels;
+
+namespace InteractiveSeven.Core.Moods
+{
+    public class DangerMood : BaseMood
+    {
+        private readonly IStatusAccessor _statusAccessor;
+        private readonly PartyStatusViewModel _partyStatus;
+        private readonly IBattleInfoAccessor _battleInfoAccessor;
+
+        public DangerMood(IStatusAccessor statusAccessor, PartyStatusViewModel partyStatus,
+            IBattleInfoAccessor battleInfoAccessor) : base("Danger Mood")
+        {
+            _statusAccessor = statusAccessor;
+            _partyStatus = partyStatus;
+            _battleInfoAccessor = battleInfoAccessor;
+        }
+
+        public override int Id => 3;
+        public override void ApplyEffect()
+        {
+            if (IsBattleActive())
+            {
+                var (_, _, hasHaste) = _partyStatus.CheckTargetValidity(Allies.All, StatusEffects.Haste);
+                foreach (Allies hasted in hasHaste)
+                {
+                    _statusAccessor.RemoveActorStatus(hasted, StatusEffects.Haste);
+                }
+                var (valid, _, _) = _partyStatus.CheckTargetValidity(Allies.All, StatusEffects.Slow);
+                foreach (Allies target in valid)
+                {
+                    _statusAccessor.SetActorStatus(target, StatusEffects.Slow);
+                }
+            }
+        }
+
+        protected bool IsBattleActive()
+        {
+            var ff7BattleMap = _battleInfoAccessor.GetBattleMap();
+            return ff7BattleMap.IsActiveBattle && !ff7BattleMap.IsBattleEnding;
+        }
+    }
+}
---
+++ src/InteractiveSeven.Core/Moods/NormalMood.cs
@@ -0,0 +1,14 @@
+﻿namespace InteractiveSeven.Core.Moods
+{
+    public class NormalMood : BaseMood
+    {
+        public NormalMood() : base("Normal Mood")
+        {
+        }
+
+        public override int Id => 1;
+        public override void ApplyEffect()
+        {
+        }
+    }
+}
