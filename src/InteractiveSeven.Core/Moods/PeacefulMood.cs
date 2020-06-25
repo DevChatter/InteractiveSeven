e7d81a50bd5e224708b7a662952f795d4907522c
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-06-24 22:40:44 -0400

    Fixing broken build

--- src/InteractiveSeven.Core/Moods/PeacefulMood.cs
+++ src/InteractiveSeven.Core/Moods/PeacefulMood.cs
@@ -4,7 +4,7 @@ using InteractiveSeven.Core.ViewModels;
 
 namespace InteractiveSeven.Core.Moods
 {
-    public class PeacefulMood : Mood<>
+    public class PeacefulMood : Mood
     {
         private readonly IStatusAccessor _statusAccessor;
         private readonly PartyStatusViewModel _partyStatus;
