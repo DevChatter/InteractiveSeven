d28c4d14a19a125fe4aee5835d85be334ba7f59d
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-04-13 17:02:19 -0400

    Initial LooseChangeMood

---
+++ src/InteractiveSeven.Core/Moods/LooseChangeMood.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveSeven.Core.Battle;
+using InteractiveSeven.Core.Diagnostics.Memory;
+using InteractiveSeven.Core.ViewModels;
+
+namespace InteractiveSeven.Core.Moods
+{
+    public class LooseChangeMood : Mood
+    {
+        private readonly IGilAccessor _gilAccessor;
+
+        public LooseChangeMood(IGilAccessor gilAccessor)
+            : base("Loose Change Mood")
+        {
+            _gilAccessor = gilAccessor;
+        }
+
+        public const int DefaultId = 4;
+        public override int Id => DefaultId;
+        public override void ApplyEffect()
+        {
+            // TODO: Vary this and make configurable.
+            //       Should be something based on current money with a minimum.
+            _gilAccessor.RemoveGil(10);
+        }
+    }
+}
