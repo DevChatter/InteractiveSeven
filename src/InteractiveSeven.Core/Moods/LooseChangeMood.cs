ce6b105cd520a763deb298758e7b9dd7375a4e12
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-04-16 17:42:53 -0400

    variable loose change

--- src/InteractiveSeven.Core/Moods/LooseChangeMood.cs
+++ src/InteractiveSeven.Core/Moods/LooseChangeMood.cs
@@ -18,9 +18,26 @@ namespace InteractiveSeven.Core.Moods
         public override int Id => DefaultId;
         public override void ApplyEffect()
         {
-            // TODO: Vary this and make configurable.
-            //       Should be something based on current money with a minimum.
-            _gilAccessor.RemoveGil(10);
+            // TODO: make configurable. calculate better
+            _gilAccessor.RemoveGil(GetAmount());
+        }
+
+        private uint GetAmount()
+        {
+            var gil = _gilAccessor.GetGil();
+            if (gil > 1000000)
+            {
+                return 2000;
+            }
+            if (gil > 500000)
+            {
+                return 1000;
+            }
+            if (gil > 100000)
+            {
+                return 200;
+            }
+            return 100;
         }
     }
 }
