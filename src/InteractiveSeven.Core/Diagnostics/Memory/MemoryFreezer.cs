0ee7180b1d29fe5708b77e9baa96e2e17ac19831
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-05-10 17:51:44 -0400

    Working on the Nothing Mood

---
+++ src/InteractiveSeven.Core/Diagnostics/Memory/MemoryFreezer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using InteractiveSeven.Core.Settings;
+
+namespace InteractiveSeven.Core.Diagnostics.Memory
+{
+    public class MemoryFreezer
+    {
+        private readonly IMemoryAccessor _memoryAccessor;
+        private ApplicationSettings Settings => ApplicationSettings.Instance;
+
+        private readonly List<Freeze> _frozenValues = new List<Freeze>();
+
+        private bool _isFrozen = true;
+
+        public MemoryFreezer(IMemoryAccessor memoryAccessor)
+        {
+            _memoryAccessor = memoryAccessor;
+        }
+
+        public void AddValue(MemLoc memLoc, byte[] value)
+        {
+            _frozenValues.Add(new Freeze(memLoc, value));
+        }
+
+        public void ThreadStart()
+        {
+            while (_isFrozen)
+            {
+                Thread.Sleep(10);
+                foreach (Freeze freeze in _frozenValues)
+                {
+                    _memoryAccessor.WriteMem(Settings.ProcessName, freeze.MemLoc.Address, freeze.Value);
+                }
+            }
+        }
+
+        public void Thaw()
+        {
+            _isFrozen = false;
+        }
+    }
+
+    record Freeze(MemLoc MemLoc, byte[] Value);
+}
---
+++ src/InteractiveSeven.Core/Moods/NothingMood.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using InteractiveSeven.Core.Diagnostics.Memory;
+using InteractiveSeven.Core.FinalFantasy;
+
+namespace InteractiveSeven.Core.Moods
+{
+    public class NothingMood : Mood
+    {
+        private bool _isStarted = false;
+        private static readonly object Padlock = new();
+        private Thread _thread;
+        private readonly MemoryFreezer _memoryFreezer;
+
+        public NothingMood(MemoryFreezer memoryFreezer)
+            : base("Nothing Mood")
+        {
+            _memoryFreezer = memoryFreezer;
+            _memoryFreezer.AddValue(Addresses.BattleApGain, new byte[] { 0, 0 });
+            _memoryFreezer.AddValue(Addresses.BattleXpGain, new byte[] { 0, 0, 0, 0 });
+            _memoryFreezer.AddValue(Addresses.BattleGilGain, new byte[] { 0, 0, 0, 0 });
+            _memoryFreezer.AddValue(Addresses.RewardItem1Amount, new byte[] { 0, 0 });
+            _memoryFreezer.AddValue(Addresses.RewardItem2Amount, new byte[] { 0, 0 });
+            _memoryFreezer.AddValue(Addresses.RewardItem3Amount, new byte[] { 0, 0 });
+        }
+
+        public const int DefaultId = 5;
+        public override int Id => DefaultId;
+        public override void ApplyEffect()
+        {
+            // ReSharper disable once InconsistentlySynchronizedField
+            if (!_isStarted)
+            {
+                lock (Padlock)
+                {
+                    if (!_isStarted)
+                    {
+                        _isStarted = true;
+                        _thread = new(_memoryFreezer.ThreadStart);
+                        _thread.Start();
+                    }
+                }
+            }
+        }
+
+        public override void RemoveEffect()
+        {
+            _memoryFreezer.Thaw();
+            _isStarted = false;
+            _thread = null;
+            base.RemoveEffect();
+        }
+    }
+}
