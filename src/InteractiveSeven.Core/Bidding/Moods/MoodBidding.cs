444eee657b105af0c6e11805e73db844355faaa6
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-05-10 17:51:44 -0400

    Working on the Nothing Mood

--- src/InteractiveSeven.Core/Bidding/Moods/MoodBidding.cs
+++ src/InteractiveSeven.Core/Bidding/Moods/MoodBidding.cs
@@ -33,11 +33,12 @@ namespace InteractiveSeven.Core.Bidding.Moods
             MoodBids.Add(new MoodBid(DangerMood.DefaultId, 0));
             MoodBids.Add(new MoodBid(PeacefulMood.DefaultId, 0));
             MoodBids.Add(new MoodBid(LooseChangeMood.DefaultId, 0));
+            MoodBids.Add(new MoodBid(LooseChangeMood.DefaultId, 0));
         }
 
         public void ResetBids(int topMoodId)
         {
-            List<BidRecord> refunds = new List<BidRecord>();
+            List<BidRecord> refunds = new();
             foreach (MoodBid nonWinner in MoodBids.Where(x => x.MoodId != topMoodId))
             {
                 refunds.AddRange(nonWinner.BidRecords);
