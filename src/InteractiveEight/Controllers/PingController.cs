c39a8eebcb9d19f854b481b72e2eb7b891c3b671
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-05-21 15:46:13 -0400

    PingController

---
+++ src/InteractiveEight/Controllers/PingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InteractiveEight.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Pong";
+        }
+    }
+}
