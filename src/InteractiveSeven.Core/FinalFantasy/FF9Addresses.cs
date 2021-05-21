1e21b2c5bc837bb3069a32abb0e938ddcb1aa688
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-05-21 12:18:33 -0400

    Starting on FF9 Addresses

---
+++ src/InteractiveSeven.Core/FinalFantasy/FF9Addresses.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveSeven.Core.Diagnostics.Memory;
+
+namespace InteractiveSeven.Core.FinalFantasy
+{
+    public class FF9Addresses
+    {
+        public MemLoc Gil { get; set; } = new MemLoc(0x25CA660C, 4);
+        public MemLoc ZidaneName { get; set; } = new MemLoc(0x27E2DDD4); // Probably wrong.
+    }
+}
