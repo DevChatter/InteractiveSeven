9809368419a7f76d1d3d2c4c2e11e681e2f9c950
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2021-05-26 22:29:09 -0400

    ReadMem from MemLoc

--- src/InteractiveSeven.Core/Diagnostics/Memory/IMemoryAccessor.cs
+++ src/InteractiveSeven.Core/Diagnostics/Memory/IMemoryAccessor.cs
@@ -4,6 +4,7 @@ namespace InteractiveSeven.Core.Diagnostics.Memory
 {
     public interface IMemoryAccessor
     {
+        byte[] ReadMem(string processName, MemLoc address);
         bool ReadMem(string processName, IntPtr address, byte[] buffer);
         void WriteMem(string processName, IntPtr address, byte[] bytes);
         ScanResult ScanMem(string processName, IntPtr startAddr,
