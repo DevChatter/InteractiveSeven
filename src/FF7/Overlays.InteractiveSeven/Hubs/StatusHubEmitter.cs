374708cba2813d88ca3aca4b07059ee966f76d75
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-09-29 14:35:48 -0400

    Files changed a year ago - no idea

---
+++ src/FF7/Overlays.InteractiveSeven/Hubs/StatusHubEmitter.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using InteractiveSeven.Core.Emitters;
+using InteractiveSeven.Core.Models;
+using InteractiveSeven.Core.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Overlays.InteractiveSeven.Hubs
+{
+    public class StatusHubEmitter : IStatusHubEmitter
+    {
+        private readonly IHubContext<StatusHub, IStatusNotification> _statusHubContext;
+
+        public StatusHubEmitter(IHubContext<StatusHub, IStatusNotification> statusHubContext)
+        {
+            _statusHubContext = statusHubContext;
+        }
+
+        public Task ShowNewPartyStatus(PartyStatusViewModel partyStatus)
+        {
+            return _statusHubContext.Clients.All.PartyStatusChanged(partyStatus);
+        }
+
+        public Task ShowNewColors(MenuColors menuColors)
+        {
+            return _statusHubContext.Clients.All.ColorChanged(
+                ColorTranslator.ToHtml(menuColors.TopLeft),
+                ColorTranslator.ToHtml(menuColors.TopRight),
+                ColorTranslator.ToHtml(menuColors.BotLeft),
+                ColorTranslator.ToHtml(menuColors.BotRight)
+            );
+        }
+
+        public Task ShowEvent(string eventText, string subText, string soundFile = null)
+        {
+            return _statusHubContext.Clients.All.ShowEvent(eventText, subText, soundFile);
+        }
+    }
+}
---
+++ src/FF7/Overlays.InteractiveSeven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Overlays.InteractiveSeven
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new InvalidOperationException("Run through Interactive Seven WPF App Only");
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+    }
+}
