7849bf3e4ff151c9b93d78e91cc28592cd24650b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-09-29 14:35:48 -0400

    Files changed a year ago - no idea

---
+++ src/FF7/Overlays.InteractiveSeven/Hubs/StatusHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using InteractiveSeven.Core.Emitters;
+using InteractiveSeven.Core.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Overlays.InteractiveSeven.Hubs
+{
+    public class StatusHub : Hub<IStatusNotification>
+    {
+        private readonly MenuColorViewModel _menuColorViewModel;
+        private readonly IStatusHubEmitter _statusHubEmitter;
+
+        public StatusHub(MenuColorViewModel menuColorViewModel, IStatusHubEmitter statusHubEmitter)
+        {
+            _menuColorViewModel = menuColorViewModel;
+            _statusHubEmitter = statusHubEmitter;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _statusHubEmitter.ShowNewColors(_menuColorViewModel.PreviewImage);
+            return base.OnConnectedAsync();
+        }
+    }
+
+    public interface IStatusNotification
+    {
+        Task PartyStatusChanged(PartyStatusViewModel partyStatus);
+        Task ColorChanged(string topLeft, string topRight, string botLeft, string botRight);
+        Task ShowEvent(string eventText, string subText, string soundFile);
+    }
+}
---
+++ src/FF7/Overlays.InteractiveSeven/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Overlays.InteractiveSeven
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSignalR();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Overlays.InteractiveSeven", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Overlays.InteractiveSeven v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
